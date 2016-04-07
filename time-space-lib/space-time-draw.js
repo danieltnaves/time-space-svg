6478c9443d9ae607a69f5653076b63deff0455d1
Daniel Thales Naves <danithales@gmail.com>
2016-04-07 00:24:54 +0000

    Add time

--- time-space-lib/space-time-draw.js
+++ time-space-lib/space-time-draw.js
@@ -5,8 +5,6 @@
 
   function SpaceTimeDraw(elementId, strokeWidth, strokeColor, parsedElements) {
 
-    //this.paperHeight   = paperWidth;
-    //this.paperWidth    = paperHeight; 
     this.elementId      = elementId;
     this.strokeWidth    = strokeWidth;
     this.strokeColor    = strokeColor;
@@ -40,7 +38,7 @@
       for (var i = 0; i < this.actors.length; i++) {
         var marginBottom = 10 + marginCounter;
         this.lanes[i] = paper.line(0, marginBottom, 0, marginBottom).attr({strokeWidth:this.strokeWidth, stroke:this.strokeColor, strokeLinecap:"round", markerEnd: marker});
-        elementNames[i] = paper.text(920,marginCounter + 15,this.actors[i]).attr({fill: this.strokeColor, fontFamily: "Arial"});
+        elementNames[i] = paper.text(920,marginCounter + 15,this.actors[i]).attr({fill: this.strokeColor, fontFamily: "Arial", fontStyle: "italic", fontWeight: "bold"});
         this.lanes[i].animate({ x1: 10, x2:900}, 1000, mina.easein); 
         marginCounter += 100;  
       }
@@ -50,16 +48,14 @@
       for (var i = 0; i < parsedElements.length; i++) {
         var senderVerticalPosition     = ((this.actors.indexOf(parsedElements[i].getSenderName())) * 100) + 10;
         var receiverVerticalPosition   = ((this.actors.indexOf(parsedElements[i].getReceiverName())) * 100) + 10;
-        console.log('senderVerticalPosition: ' + senderVerticalPosition); 
-        console.log('receiverVerticalPosition: ' + receiverVerticalPosition);
         var senderHorizontalPosition   = (parsedElements[i].getSenderTime()) * 10;
         var receiverHorizontalPosition = (parsedElements[i].getReceiverTime()) * 10; 
-        console.log('senderHorizontalPosition: ' + senderHorizontalPosition);  
-        console.log('receiverHorizontalPosition: ' + receiverHorizontalPosition);
-        //console.log(senderHorizontalPosition);
+        
         //circle(x,y,r), x -> x position, y -> y position, r -> radius
-        var senderDot   = paper.circle(senderHorizontalPosition, senderVerticalPosition, 4).attr({strokeWidth:2,stroke:"black",strokeLinecap:"round", fill: "black"})
-        var receiverDot = paper.circle(receiverHorizontalPosition, receiverVerticalPosition, 4).attr({strokeWidth:2,stroke:"black",strokeLinecap:"round", fill: "black"})
+        var senderDot = paper.circle(senderHorizontalPosition, senderVerticalPosition, 4).attr({strokeWidth:2,stroke:this.strokeColor,strokeLinecap:"round", fill: this.strokeColor});
+        paper.text(senderHorizontalPosition - 7,senderVerticalPosition + 20,parsedElements[i].getSenderTime()).attr({fill: this.strokeColor, fontFamily: "Arial", fontStyle: "italic", fontSize: "11px"});
+        var receiverDot = paper.circle(receiverHorizontalPosition, receiverVerticalPosition, 4).attr({strokeWidth:2,stroke:this.strokeColor,strokeLinecap:"round", fill: this.strokeColor});
+        paper.text(receiverHorizontalPosition - 7,receiverVerticalPosition + 20,parsedElements[i].getReceiverTime()).attr({fill: this.strokeColor, fontFamily: "Arial", fontStyle: "italic", fontSize: "11px"});
       }  
     }
 
