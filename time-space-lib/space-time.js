bf4d28292dc3574d768d91420db81691e237f9c7
Daniel Naves <danithales@gmail.com>
2016-04-12 13:47:19 -0300

    Documentation generated with JSDOC

--- time-space-lib/space-time.js
+++ time-space-lib/space-time.js
@@ -5,24 +5,35 @@ var SpaceTimeDraw = require('./space-time-draw');
 
 module.exports = SpaceTime;
 
+ /**
+  * Parses and draws a space time diagrams.
+  * @constructor
+  * @param {string} text - Raw text with user input.
+  */
 function SpaceTime(text) { 
 
 	this.text           = text;
 	this.parsedElements = new Array();
 
+	/**
+    * Parses raw input and create array of space time objects.
+    */
 	this.parseInput = function()  {
 	  var spaceTimeRaw    = new SpaceTimeRaw(this.text);
 	  var linesAsArray    = spaceTimeRaw.returnLinesAsArray();
 	  this.parsedElements = spaceTimeRaw.returnRawInputAsArray(linesAsArray);
 	}
 
+	/**
+    * Sets space time diagram atributes and draws space time diagram based on user input.
+    * @param {string} elementId - SVG element ID defined in svg tag.
+    */
 	this.drawInput = function(elementId)  {
 	  this.parseInput();
 	  var spaceTimeDraw = new SpaceTimeDraw(elementId, 2, 'black', this.parsedElements);
-	  spaceTimeDraw.drawHorizontalLines(3);
+	  spaceTimeDraw.drawHorizontalLines();
 	  spaceTimeDraw.identifyLineNames();
 	  spaceTimeDraw.drawPoints();
-	  //console.log(spaceTimeDraw);
 	}
 
 }
